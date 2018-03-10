26447ebb1f95f1c61ca08a39d3d93f420e2dcaed
Jean <jeanlucafp@gmail.com>
2018-03-10 18:11:51 -0300

    projeto criado

---
+++ src/main/java/com/jean/pedidopj/PedidoPjApplication.java
@@ -0,0 +1,12 @@
+package com.jean.pedidopj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoPjApplication.class, args);
+	}
+}
