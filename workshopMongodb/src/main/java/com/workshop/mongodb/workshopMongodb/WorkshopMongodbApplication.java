062a0a312099bd2c60160c52bebb04f1db162128
jean <jeanlucafp@gmail.com>
2019-10-29 16:14:31 -0300

    workshop mongodb with spring

---
+++ workshopMongodb/src/main/java/com/workshop/mongodb/workshopMongodb/WorkshopMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.mongodb.workshopMongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongodbApplication.class, args);
+	}
+
+}
---
+++ workshopMongodb/src/test/java/com/workshop/mongodb/workshopMongodb/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.mongodb.workshopMongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
